8a855a8bd21a8e7da18b933e0cd4e836a25a5bcc
pipixia66 <55069478+pipixia66@users.noreply.github.com>
2019-09-09 21:06:52 +0800

    Add files via upload

---
+++ Day01.py
@@ -0,0 +1,63 @@
+
+#1.温度转换
+
+# C = float (input('请输入摄氏温度:'))
+# F = (9 /5) * C + 32
+# print('%.2f 华氏度' %F)
+
+
+#2.计算圆柱体的体积
+#area:面积 radius:半径 volume:体积 length:高 π=3.14
+#radius=float(input('请您输入圆柱体的半径：'))
+#length=float(input('请您输入圆柱体的高：'))
+#area= radius * length * 3.14
+#print('圆柱体的面积为： %0.4f' %area)
+#volume= area * length
+#print('圆柱体的体积为：%0.4f' %volune)
+
+#3.feet=float(input('输入一个英尺数:'))
+#meters=feet * 0.305
+#print('%0.1f英尺数为%0.4米' %（feet，meters）)
+
+#4.M=float(input('请输入水的量：'))
+#initalTemperature = float(input('输入初始温度：'))
+#finalTemperature = float(input('输入最终温度：'))
+# Q = M * （finalTemperature - initalTemperature) * 4184
+# print('所需的能量是：%0.2f' %Q)
+
+
+#5.
+#c = int(input('请输入差额':))
+#n = float(input('请输入年利率:'))
+#l = c * (n/1200)
+#print('下月要付的利息：%0.4f' %l)
+
+#6.
+# v0 = float(input（'请输入初始速度：'))
+# v1 = float(input('请输入末速度：'))
+# t = float(input('请输入占用的时间：'))
+# a = (v1 - v0)/t
+# ptint('平均加速度为： %0.4f' %a)
+
+#7.
+#a = float(inout('请输入存款数：'))
+#one = a * (1+0.00417)
+#two = (a+one)(1+0.00417)
+#three = (a+two)(1+0.00417)
+#four = (a+three)(1+0.00417)
+#five = (a+four)(1+0.00417)
+#six = (a+five)(1+0.00417)
+#print('六个月后，账户总额为：%0.2f' %six)
+
+#8.
+#n=int(input(‘请输入一个0-1000之间的整数:’))
+#x=n % 10
+#z=n // 100
+#sum=x+y+z
+#print(‘数字的和为：%d’%sum)
+# 输入一个数字判断是奇数还是偶数
+# num=int(input(‘输入一个数字：’))
+# if num % 2 == 0:
+# print (‘是偶数’)
+# else:
+# print(‘是奇数’)
